f361eb7f7cac3a2ce5114f464037fa8dc5d6165d
Yu Yin <136771156+Nimbus318@users.noreply.github.com>
2025-12-03 19:15:12 +0800

    feat: add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Caveat } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { OSProvider } from "@/context/OSContext";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           {children}
         </OSProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
